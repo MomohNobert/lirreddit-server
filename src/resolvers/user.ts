abd9e2d29e1bdb30cb8ccb708720ea6d6c3d8ac4
Momoh Nobert <okhai0112@gmail.com>
2020-11-30 12:52:11 +0100

    modifications

--- src/resolvers/user.ts
+++ src/resolvers/user.ts
@@ -32,7 +32,7 @@ export class UserResolver {
     async changePassword(
         @Arg('token') token: string,
         @Arg('newPassword') newPassword: string,
-        @Ctx() { redis, em }: MyContext
+        @Ctx() { req, redis, em }: MyContext
     ): Promise<UserResponse> {
         
         if (newPassword.length <= 5) {
@@ -44,7 +44,8 @@ export class UserResolver {
             ]}
         }
 
-        const userId = await redis.get(FORGET_PASSWORD_PREFIX + token);
+        const key = FORGET_PASSWORD_PREFIX + token
+        const userId = await redis.get(key);
         if (!userId) {
             return  { errors : [
                 {
@@ -67,6 +68,9 @@ export class UserResolver {
         user.password = await argon2.hash(newPassword);
         await em.persistAndFlush(user);
 
+        await redis.del(key);
+
+        req.session.userId = user.id
         return { user };
         
     }
--- src/types.ts
+++ src/types.ts
@@ -3,10 +3,11 @@ import { IDatabaseDriver } from "@mikro-orm/core/drivers/IDatabaseDriver";
 import { EntityManager } from "@mikro-orm/core/EntityManager";
 import { Request, Response } from "express";
 import { Redis } from "ioredis";
+import { Session, SessionData } from 'express-session'
 
 export type MyContext = {
     em: EntityManager<any> & EntityManager<IDatabaseDriver<Connection>>;
-    req: Request & {session: Express.Session };
+    req: Request & { session: Session & Partial<SessionData> & { userId?: number } }
     res: Response;
     redis: Redis;
 }
